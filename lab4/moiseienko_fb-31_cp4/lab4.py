5a6a0c968675e9c0bf35d047395af5734375cc28
Dmytro864 <dmytrmoise-ipt27@lll.kpi.ua>
2026-04-26 20:08:14 +0200

    Final code and pdf report

--- lab4/moiseienko_fb-31_cp4/lab4.py
+++ lab4/moiseienko_fb-31_cp4/lab4.py
@@ -64,3 +64,87 @@ def GenerateKeyPair(bit_length=256):
     d %= phi
 
     return {"public": (e, n), "private": (d, p, q)}
+
+def Encrypt(message, e,  n):
+    return power_mod(message, e, n)
+
+def Decrypt(ciphertext, d, n):
+    return power_mod(ciphertext, d, n)
+
+def Sign(message, d, n):
+    return power_mod(message, d, n)
+
+def Verify(message, signature, e, n):
+    return message == power_mod(signature, e, n)
+
+def SendKey(k, d_A, n_A, e_B, n_B):
+    k1 = power_mod(k, e_B, n_B)
+
+    S = Sign(k, d_A, n_A)
+
+    S1 = power_mod(S, e_B, n_B)
+
+    return k1, S1
+
+def ReceiveKey(k1, S1, d_B, n_B, e_A, n_A):
+    k = power_mod(k1, d_B, n_B)
+
+    S = power_mod(S1, d_B, n_B)
+
+    is_authentic = Verify(k, S, e_A, n_A)
+
+    return k, is_authentic
+
+def main():
+    print("Генерація ключів для абонентів A та B")
+    while True:
+        key_A = GenerateKeyPair(256)
+        key_B = GenerateKeyPair(256)
+
+        n_A = key_A["public"][1]
+        n_B = key_B["public"][1]
+
+        if n_B >= n_A:
+            break
+
+    e_A, n_A = key_A["public"]
+    d_A, p_A, q_A = key_A["private"]
+
+    e_B, n_B = key_B["public"]
+    d_b, p_B, q_B = key_B["private"]
+
+    print(f"Абонент A:\n p={p_A}\n q={q_A}\n n={n_A}\n e={e_A}\n d={d_A}\n")
+    print(f"Абонент B:\n p={p_B}\n q={q_B}\n n={n_B}\n e={e_B}\n d={d_b}\n")
+
+    print("Шифрування та Цифровий підпис")
+    M = random.randint(100, n_A - 1)
+    print(f"Відкрите повідомлення M: {M}")
+
+    C = Encrypt(M, e_B, n_B)
+    print(f"Шифртекст (C): {C}")
+
+    M_decrypted = Decrypt(C, d_b, n_B)
+    print(f"Розшифроване повідомлення: {M_decrypted}")
+    print(f"Перевірка розшифрування: {'Успішно' if M == M_decrypted else 'Помилка'}")
+
+    S = Sign(M, d_A, n_A)
+    print(f"Цифровий підпис (S): {S}")
+
+    is_valid = Verify(M, S, e_A, n_A)
+    print(f"Перевірка підпису: {'Вірний' if is_valid else 'Невірний'}")
+
+    print("\nПротокол конфіденційного розсилання ключів")
+    k = random.randint(100, n_A - 1)
+    print(f"Секретний ключ k для передачі: {k}")
+
+    k1, S1 = SendKey(k, d_A, n_A, e_B, n_B)
+    print(f"Характеристики передачі:\n k1 (шифр. ключ)={k1}\n S1 (шифр. підпис)={S1}")
+
+    received_k, authentic = ReceiveKey(k1, S1, d_b, n_B, e_A, n_A)
+    print(f"Отриманий ключ k: {received_k}")
+    print(f"Автентифікація відправника: {'Підтверджено' if authentic else 'Відхилено'}")
+
+if __name__ == "__main__":
+    main()
+
+
